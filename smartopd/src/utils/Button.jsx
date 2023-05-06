47d16306bde65ed7978238432166893eceafabfa
Ebenezer <agyekumebenezer2001@gmail.com>
2023-05-06 11:18:26 +0000

    made responsive

--- smartopd/src/utils/Button.jsx
+++ smartopd/src/utils/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Button = (props) => {
   return (
     <button
-      className="bg-main-bg font-semibold text-white font-[Poppins] py-2  w-[171.39px] h-[55px]  hover:bg-main-bg
+      className="bg-main-bg font-semibold text-white mt-8 md:mt-0  font-[Poppins] py-2  w-[171.39px] h-[55px]  hover:bg-main-bg
     duration-500"
     >
       {props.children}
