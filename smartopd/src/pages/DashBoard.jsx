a5de959cd98d4b1b508906c76520e67e2d6e3aa1
Ebenezer <agyekumebenezer2001@gmail.com>
2023-05-18 15:37:51 +0000

    dashboard

--- smartopd/src/pages/DashBoard.jsx
+++ smartopd/src/pages/DashBoard.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import "../utils/avatar.css";
 import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
+import { ServiceCard } from "./ServiceCard";
+
+import weclDoc from "../images/newStock.png";
+
 export const DashBoard = () => {
   return (
     <>
       <Navbar />
-      <div className="container flex">
-        <div className="w-[90%] lg:w-2/3 bg-green-600"></div>
-        <div className="w-[90%] lg:w-1/3 bg-violet-700">he</div>
+      <div>
+        <div className="container flex">
+          <div className="w-[90%] lg:w-2/3">
+            <div className="text-5xl font-bold">Welcome to SmartODP</div>
+          </div>
+          <div className="w-[90%] lg:w-1/3 bg-pink-200 rounded">
+            <img src={weclDoc} />
+          </div>
+        </div>
+        <div className="text-5xl font-bold text-center pt-10">
+          We are coming Soon
+        </div>
+        <ServiceCard />
+        <Footer />
       </div>
     </>
   );
